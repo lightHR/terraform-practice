755b78458e231c86443300a9e95e9ee7813ea4f3
lightHR <rajkumar.aut@gmail.com>
2024-06-28 16:01:31 +0000

    variable file and output file

--- main.tf
+++ main.tf
@@ -9,30 +9,6 @@ terraform {
 
 provider "docker" {}
 
-variable ext_port{
-  type=number
-  default=1880
-  
-  validation {
-    condition = var.ext_port <= 65535 && var.ext_port > 0
-    error_message = "The external port must be in the valid port range 0 - 65535."
-  }
-}
-
-variable int_port{
-  type=number
-  default=1880
-  
-  validation {
-    condition = var.int_port == 1880
-    error_message = "The internal port must be 1880."
-  }
-}
-
-variable container_count {
-  type = number
-  default=1
-}
 
 resource "docker_image" "nodered_image" {
   name = "nodered/node-red:latest"
@@ -57,14 +33,4 @@ resource "docker_container" "nodered_container" {
   }
 }
 
-
-output "conainer-name" {
-  value = docker_container.nodered_container[*].name
-  description = "This is name of the container"
-}
-
-output "ip-address" {
-  value = [for i in docker_container.nodered_container[*]: join(":", [i.network_data[0].ip_address,i.name])]
-  description = "The ip address for the container2."
-}
 
---
+++ output.tf
@@ -0,0 +1,10 @@
+output "conainer-name" {
+  value = docker_container.nodered_container[*].name
+  description = "This is name of the container"
+}
+
+output "ip-address" {
+  value = [for i in docker_container.nodered_container[*]: join(":", [i.network_data[0].ip_address,i.name])]
+  description = "The ip address for the container2."
+}
+
