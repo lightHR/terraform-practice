9f7de3113d76f1a3ccad29e9a30e65369b98fb27
lightHR <rajkumar.aut@gmail.com>
2024-06-28 22:40:21 +0000

    map and lookup

--- main.tf
+++ main.tf
@@ -9,36 +9,36 @@ terraform {
 
 provider "docker" {}
 
-resource "null_resource"  "dockervol" {
+resource "null_resource" "dockervol" {
   provisioner "local-exec" {
     command = "mkdir noderedvol || true && sudo chown -R 1000:1000 noderedvol/"
   }
 }
 
 resource "docker_image" "nodered_image" {
-  name = "nodered/node-red:latest"
+  name = lookup(var.image,var.env)
 }
 
-resource "random_string" "random1"{
-  count = local.container_count
-  length = 4
+resource "random_string" "random1" {
+  count   = local.container_count
+  length  = 4
   special = false
-  upper = false
+  upper   = false
   numeric = false
 }
 
 
 resource "docker_container" "nodered_container" {
   count = local.container_count
-  name  = join("-",["nodered",random_string.random1[count.index].result])
+  name  = join("-", ["nodered", random_string.random1[count.index].result])
   image = docker_image.nodered_image.image_id
   ports {
     internal = var.int_port
     external = var.ext_port[count.index]
   }
   volumes {
-    container_path ="/data"
-    host_path = "/home/ubuntu/environment/terraform-practice/noderedvol"
+    container_path = "/data"
+    host_path      = "${path.cwd}/noderedvol"
   }
 }
 
--- variable.tf
+++ variable.tf
@@ -1,18 +1,32 @@
-variable ext_port{
-  type=list(number)
-  
+variable "env" {
+  type    = string
+  default = "dev"
+}
+
+variable "image" {
+  type        = map(any)
+  description = "impage for container"
+  default = {
+    dev  = "nodered/node-red:latest"
+    prod = "nodered/node-red:latest-minimal"
+  }
+}
+
+variable "ext_port" {
+  type = list(number)
+
   validation {
-    condition = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
+    condition     = max(var.ext_port...) <= 65535 && min(var.ext_port...) > 0
     error_message = "The external port must be in the valid port range 0 - 65535."
   }
 }
 
-variable int_port{
-  type=number
-  default=1880
-  
+variable "int_port" {
+  type    = number
+  default = 1880
+
   validation {
-    condition = var.int_port == 1880
+    condition     = var.int_port == 1880
     error_message = "The internal port must be 1880."
   }
 }
