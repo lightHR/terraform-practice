a7124b69c0701c03066f313b3c1d09357298e761
lightHR <rajkumar.aut@gmail.com>
2024-07-03 02:25:51 +0000

    deploy containers with for_each

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
-output "conainer-name" {
-  value       = module.container[*].container-name
-  description = "This is name of the container"
-}
+# output "conainer-name" {
+#   value       = module.container[*].container-name
+#   description = "This is name of the container"
+# }
 
-output "ip-address" {
-  value       = flatten(module.container[*].ip-address)
-  description = "The ip address for the container2."
-}
+# output "ip-address" {
+#   value       = flatten(module.container[*].ip-address)
+#   description = "The ip address for the container2."
+# }
 
--- variable.tf
+++ variable.tf
@@ -16,25 +16,25 @@ variable "image" {
 variable "ext_port" {
   type = map
 
-  validation {
-    condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
-    error_message = "The external port must be in the valid port range 1980 - 65535."
-  }
+  # validation {
+  #   condition     = max(var.ext_port["dev"]...) <= 65535 && min(var.ext_port["dev"]...) >= 1980
+  #   error_message = "The external port must be in the valid port range 1980 - 65535."
+  # }
   
-   validation {
-    condition     = max(var.ext_port["prod"]...) <= 1980 && min(var.ext_port["prod"]...) >= 1880
-    error_message = "The external port must be in the valid port range 1880- 1980."
-  }
+  # validation {
+  #   condition     = max(var.ext_port["prod"]...) <= 1980 && min(var.ext_port["prod"]...) >= 1880
+  #   error_message = "The external port must be in the valid port range 1880- 1980."
+  # }
 }
 
 variable "int_port" {
   type    = number
   default = 1880
 
-  validation {
-    condition     = var.int_port == 1880
-    error_message = "The internal port must be 1880."
-  }
+  # validation {
+  #   condition     = var.int_port == 1880
+  #   error_message = "The internal port must be 1880."
+  # }
 }
 
 # errored - function connot applied in variable so replacing this with locals
@@ -43,8 +43,8 @@ variable "int_port" {
 #   default= length(var.ext_port)
 # }
 
-locals {
-  # container_count = length(lookup(var.ext_port,terraform.workspace))
-  container_count = length(var.ext_port[terraform.workspace])
-}
+# locals {
+#   # container_count = length(lookup(var.ext_port,terraform.workspace))
+#   container_count = length(var.ext_port[terraform.workspace])
+# }
 
